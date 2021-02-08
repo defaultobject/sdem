52afadc34672f972bdab133e3716dfe1e9c78447
Ollie <ohamelijnck@gmail.com>
2021-02-08 18:15:41 +0000

    :art: refactor load_experiment_config

--- model_log/computation/manager.py
+++ model_log/computation/manager.py
@@ -38,6 +38,37 @@ def set_custom_import():
 def reset_import():
     builtins.__import__ = old_imp
 
+def get_experiment_config():
+    """
+        There are three levels of config: local, project, global.
+        Local takes precedent over project + gloabl, and project does over global.
+    """
+    tmpl = template.get_template()
+
+    _config = {}
+
+    def read_config(file_name):
+        try:
+            c = utils.read_yaml(file_name)
+        except Exception as e:
+            c = {}
+
+        return c
+
+    #try load global config
+    global_config = read_config(tmpl['global_config'])
+    _config = utils.add_dicts([_config, global_config])
+
+    #try load project config
+    project_config = read_config(tmpl['project_config'])
+    _config = utils.add_dicts([_config, project_config])
+
+    #try load local config
+    local_config = read_config(tmpl['local_config'])
+    _config = utils.add_dicts([_config, local_config])
+
+    return _config
+
 def get_experiment_name():
     """
         The experiment name is the same as the root folder that stores the experiment  
--- model_log/computation/startup.py
+++ model_log/computation/startup.py
@@ -1,6 +1,7 @@
 from .. import state, utils, template
-from pathlib import Path
+from . import manager
 
+from pathlib import Path
 from loguru import logger
 
 def check():
@@ -19,34 +20,8 @@ def check():
     return True
 
 def load_config():
-    """
-        There are three levels of config: local, project, global.
-        Local takes precedent over project + gloabl, and project does over global.
-    """
-
-    tmpl = template.get_template()
-
-    _config = {}
-
-    def read_config(file_name):
-        try:
-            c = utils.read_yaml(file_name)
-        except Exception as e:
-            c = {}
-
-        return c
-
-    #try load global config
-    global_config = read_config(tmpl['global_config'])
-    _config = utils.add_dicts([_config, global_config])
-
-    #try load project config
-    project_config = read_config(tmpl['project_config'])
-    _config = utils.add_dicts([_config, project_config])
-
-    #try load local config
-    local_config = read_config(tmpl['local_config'])
-    _config = utils.add_dicts([_config, local_config])
+    
+    _config = manager.get_experiment_config()
 
     if state.verbose:
         logger.info('Experiment config: ')
