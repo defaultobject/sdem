1dede62e46e3780e07eacf2b58caf2927c439369
Ollie <ohamelijnck@gmail.com>
2021-02-11 16:01:57 +0000

    rename to sem

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ import setuptools
 
 
 setuptools.setup(
-    name="model_log", 
+    name="sem", 
     version="0.0.1",
     author="O Hamelijnck",
     author_email="ohamelijnck@turing.ac.uk;",
-    description="Experiment manager",
+    description="Sacred Experiment Manager",
     long_description="",
     long_description_content_type="text/markdown",
     url="N/A",
@@ -18,5 +18,5 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    scripts=["cli/mono"]
+    scripts=["cli/sem"]
 )
