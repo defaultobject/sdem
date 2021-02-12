5c837f2356e19d1b0614910afc35183f14da9ca8
Ollie <ohamelijnck@gmail.com>
2021-02-12 14:17:46 +0000

    update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setuptools.setup(
     name="sdem", 
-    version="0.1.0",
+    version="0.1.1",
     author="O Hamelijnck",
     author_email="ohamelijnck@gmail.com",
     description="Sacred Experiment Manager",
