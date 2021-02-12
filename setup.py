6d86209ae567fc58cee1298645fbc248fc9d3af7
Ollie <ohamelijnck@gmail.com>
2021-02-12 09:31:13 +0000

    add requirements into setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,10 @@ setuptools.setup(
     name="sdem", 
     version="0.1.0",
     author="O Hamelijnck",
-    author_email="ohamelijnck@turing.ac.uk;",
+    author_email="ohamelijnck@gmail.com",
     description="Sacred Experiment Manager",
     long_description="",
     long_description_content_type="text/markdown",
-    url="N/A",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -18,5 +17,13 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    scripts=["cli/sdem"]
+    install_requires=[
+        'typer==0.3.2',
+        'loguru==0.5.3',
+        'slurmjobs==0.0.15',
+        'sacred==0.8.1',
+        'seml==0.2.4',
+        'scikit-learn==0.23.2'
+    ],
+    scripts=["cli/sdem"],
 )
