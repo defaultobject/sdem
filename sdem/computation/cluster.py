6c8833fc4233f0e4f3af07f6b54d19f8e4ea54cf
Ollie <ohamelijnck@gmail.com>
2021-02-15 11:42:12 +0000

    remove exit

--- sdem/computation/cluster.py
+++ sdem/computation/cluster.py
@@ -178,7 +178,6 @@ def compress_files_for_cluster(configs_to_run, run_settings, experiment_name, cl
     utils.zip_dir(folder_to_move, zipf, ignore_dir_arr=folders_to_ignore)
 
     zipf.close()
-    exit()
 
 def move_files_to_cluster(configs_to_run, run_settings, experiment_name, cluster_config):
 
