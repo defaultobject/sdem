bb479a2cce98c954169612b11757e0080824677d
Oliver Hamelijnck <ohamelijnck@turing.ac.uk>
2023-08-29 13:47:55 +0100

    revert setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    package_dir = {'': 'sdem'},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
